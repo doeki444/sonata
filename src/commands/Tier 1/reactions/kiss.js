e151ee3e956159b3f2cf10d1ddc44f5bb0b6a8e4
Vlad Ciphersky <32297717+vladciphersky@users.noreply.github.com>
2020-02-03 09:48:35 +0500

    Create kiss.js

---
+++ src/commands/Tier 1/reactions/kiss.js
@@ -0,0 +1,35 @@
+const utils = require('../../utils.js');
+const { Embed, Command } = require('discore.js');
+
+class MyCommand extends Command {
+    get options() {
+        return {
+            enabled: true,
+            name: 'kiss',
+            description: 'Поцеловать пользователя',
+            usage: '<@user/ID>'
+        };
+    }
+
+    get customOptions() {
+        return {
+            tier: 1,
+            category: 'reactions'
+        };
+    }
+
+    async run(message, args) {
+		let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+		if(!member)
+			return utils.error(message, 'NO_ARGS`, { usage: `${this.client.prefix + this.options.name} ${this.options.usage}` });
+	
+        let embed = new Embed()
+        	.setColor('#7289DA')
+            .setAuthor(`${message.author.username} поцеловал ${member.user.username}`, message.author.displayAvatarURL)
+            .setImage(await utils.neko.sfw.kiss().url);
+
+        return message.channel.send(embed);
+    }
+};
+
+module.exports = MyCommand;
