25a7593b830cd81521e31fdf0019462bbb801383
Vlad Ciphersky <32297717+vladciphersky@users.noreply.github.com>
2020-02-04 06:29:37 +0500

    Update bug.js

--- src/commands/Tier 1/bug.js
+++ src/commands/Tier 1/bug.js
@@ -2,8 +2,27 @@ const { date, fridaySnippet } = require('../utils');
 const { Command, Embed } = require('discore.js');
 
 class MyCommand extends MyCommand {
+    get options() {
+        return {
+            enabled: true,
+            name: 'bug',
+            description: 'Сообщить о возможно найденном баге',
+            usage: '<сообщение>'
+        };
+    }
+
+    get customOptions() {
+        return {
+            tier: 1,
+            category: 'other'
+        };
+    }
+
     run(message, args) {
         let bug = args.join(" ");
+        if(!bug)
+            return utils.error(message, 'NO_ARGS', { usage: `${this.client.prefix + this.options.name} ${this.options.usage}` });
+
         let guild = message.guild;
         let servers = this.client.guilds.size;
         let nouns = ['сервер', 'сервера', 'серверов'];
@@ -16,7 +35,7 @@ class MyCommand extends MyCommand {
             .addField("Администратор сервера", `${guild.owner.user} (ID: ${guild.owner.user.id})`)
             .addField("Дата создания сервера", date(guild.createdAt).locale("ru").format("LLLL"))
     
-        return this.client.channels.get(process.env.NEW_SERVERS_CHANNEL).send(embed);
+        return this.client.channels.get(process.env.NEW_SERVERS_CHANNEL).send(`<@178404926869733376>`, embed);
     }
 };
 
